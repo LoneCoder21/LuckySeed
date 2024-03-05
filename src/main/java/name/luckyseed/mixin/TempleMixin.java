9b7c42c2f72c690f08bcda6ab91c9b132a9fcf7a
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-03-05 09:48:56 -0800

    Generate Lava Lakes on surface alongside the desert pyramid

--- src/main/java/name/luckyseed/mixin/TempleMixin.java
+++ src/main/java/name/luckyseed/mixin/TempleMixin.java
@@ -1,5 +1,6 @@
 package name.luckyseed.mixin;
 
+import net.minecraft.block.Blocks;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.structure.DesertTempleGenerator;
@@ -10,6 +11,8 @@ import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.ChunkRegion;
 import net.minecraft.world.gen.StructureAccessor;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.feature.LakeFeature;
+import net.minecraft.world.gen.feature.SingleStateFeatureConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
@@ -25,7 +28,6 @@ public class TempleMixin {
     @Inject(at = @At("HEAD"), method = "generate(Lnet/minecraft/world/StructureWorldAccess;Lnet/minecraft/world/gen/StructureAccessor;Lnet/minecraft/world/gen/chunk/ChunkGenerator;Ljava/util/Random;Lnet/minecraft/util/math/BlockBox;Lnet/minecraft/util/math/ChunkPos;Lnet/minecraft/util/math/BlockPos;)Z")
     private void detectTemple(StructureWorldAccess structureWorldAccess, StructureAccessor structureAccessor, ChunkGenerator chunkGenerator, Random random, BlockBox boundingBox, ChunkPos chunkPos, BlockPos blockPos, CallbackInfoReturnable<Boolean> cir) {
         Logger LOGGER = LogManager.getLogger("luckyseed");
-        LOGGER.info("Temple detected!");
 
         if (!(structureWorldAccess instanceof ChunkRegion)) {
             LOGGER.info("not chunk region");
@@ -39,7 +41,10 @@ public class TempleMixin {
 
         for(ServerPlayerEntity player: players) {
             player.requestTeleport(blockPos.getX(), blockPos.getY(), blockPos.getZ());
-            LOGGER.info("Teleported player");
         }
+
+        SingleStateFeatureConfig config = new SingleStateFeatureConfig(Blocks.LAVA.getDefaultState());
+        LakeFeature feature = new LakeFeature(SingleStateFeatureConfig.CODEC);
+        feature.generate(structureWorldAccess, chunkGenerator, random, blockPos, config);
     }
 }
