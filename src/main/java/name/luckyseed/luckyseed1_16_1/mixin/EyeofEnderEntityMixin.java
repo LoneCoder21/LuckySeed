1ee11be6821a4848c4fd2cb4cb2cfee097de1946
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-03-11 19:27:52 -0700

    make eye of ender always drop

---
+++ src/main/java/name/luckyseed/luckyseed1_16_1/mixin/EyeofEnderEntityMixin.java
@@ -0,0 +1,20 @@
+package name.luckyseed.luckyseed1_16_1.mixin;
+
+import net.minecraft.entity.EyeOfEnderEntity;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EyeOfEnderEntity.class)
+public class EyeofEnderEntityMixin {
+    @Shadow
+    private boolean dropsItem;
+
+    @Inject(method = "moveTowards(Lnet/minecraft/util/math/BlockPos;)V", at = @At("TAIL"))
+    private void disableEyeBreak(BlockPos pos, CallbackInfo ci) {
+        this.dropsItem = true;
+    }
+}
