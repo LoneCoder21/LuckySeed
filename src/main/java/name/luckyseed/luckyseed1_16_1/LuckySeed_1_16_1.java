6a4cd1d0efee8af2aa1743642473cf419957dbcc
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-03-11 18:51:23 -0700

    maximize blaze rates + flint from gravel

--- src/main/java/name/luckyseed/luckyseed1_16_1/LuckySeed_1_16_1.java
+++ src/main/java/name/luckyseed/luckyseed1_16_1/LuckySeed_1_16_1.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
 import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.Items;
 import net.minecraft.loot.ConstantLootTableRange;
 import net.minecraft.loot.LootTables;
@@ -20,6 +22,11 @@ public class LuckySeed_1_16_1 implements ModInitializer  {
     public static final Logger LOGGER = LogManager.getLogger("luckyseed");
     private static final Identifier PYRAMID_CHEST_LOOT_TABLE_ID = LootTables.DESERT_PYRAMID_CHEST;
     private static final Identifier PIGLIN_BARTER_LOOT_TABLE_ID = LootTables.PIGLIN_BARTERING_GAMEPLAY;
+    private static final Identifier BLAZE_LOOT_TABLE_ID = EntityType.BLAZE.getLootTableId();
+
+    private static final Identifier ENDERMEN_LOOT_TABLE_ID = EntityType.ENDERMAN.getLootTableId();
+
+    private static final Identifier GRAVEL_LOOT_TABLE_ID = Blocks.GRAVEL.getLootTableId();
 
     @Override
     public void onInitialize() {
@@ -53,6 +60,30 @@ public class LuckySeed_1_16_1 implements ModInitializer  {
 
                 supplier.pool(obsidianPool);
                 supplier.pool(pearlPool);
+            }
+
+            if (BLAZE_LOOT_TABLE_ID.equals(id)) {
+                FabricLootPoolBuilder rodPool = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootTableRange.create(1))
+                        .with(ItemEntry.builder(Items.BLAZE_ROD));
+
+                supplier.pool(rodPool);
+            }
+
+            if (ENDERMEN_LOOT_TABLE_ID.equals(id)) {
+                FabricLootPoolBuilder pearlPool = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootTableRange.create(1))
+                        .with(ItemEntry.builder(Items.ENDER_PEARL));
+
+                supplier.pool(pearlPool);
+            }
+
+            if (GRAVEL_LOOT_TABLE_ID.equals(id)) {
+                FabricLootPoolBuilder flintPool = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootTableRange.create(1))
+                        .with(ItemEntry.builder(Items.FLINT));
+
+                supplier.pool(flintPool);
             }
         });
     }
