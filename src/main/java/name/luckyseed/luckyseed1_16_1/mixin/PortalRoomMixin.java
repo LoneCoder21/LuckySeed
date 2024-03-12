c735f9838138ea14515b1af2a9d0ee3828268aa5
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-03-12 09:44:36 -0700

    increase portal room eye chances

---
+++ src/main/java/name/luckyseed/luckyseed1_16_1/mixin/PortalRoomMixin.java
@@ -0,0 +1,14 @@
+package name.luckyseed.luckyseed1_16_1.mixin;
+
+import net.minecraft.structure.StrongholdGenerator;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(StrongholdGenerator.PortalRoom.class)
+public class PortalRoomMixin {
+    @ModifyConstant(method = "generate(Lnet/minecraft/world/ServerWorldAccess;Lnet/minecraft/world/gen/StructureAccessor;Lnet/minecraft/world/gen/chunk/ChunkGenerator;Ljava/util/Random;Lnet/minecraft/util/math/BlockBox;Lnet/minecraft/util/math/ChunkPos;Lnet/minecraft/util/math/BlockPos;)Z", constant = @Constant(floatValue = 0.9F))
+    private float PortalRoomOdds(float value) {
+        return 0.6f;
+    }
+}
