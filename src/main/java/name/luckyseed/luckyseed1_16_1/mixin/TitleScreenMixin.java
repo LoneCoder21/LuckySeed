4467011e1db8cbde4f3587df952f0cf955146427
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-03-14 18:01:35 -0700

    add gui texture and buttons on title screen to configure options for the mod

---
+++ src/main/java/name/luckyseed/luckyseed1_16_1/mixin/TitleScreenMixin.java
@@ -0,0 +1,32 @@
+package name.luckyseed.luckyseed1_16_1.mixin;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.options.LanguageOptionsScreen;
+import net.minecraft.client.gui.widget.TexturedButtonWidget;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin extends Screen {
+    @Unique
+    private static final Identifier WIDGETS_LOCATION = new Identifier("luckyseed", "textures/gui/widgets.png");
+
+    protected TitleScreenMixin(Text title) {
+        super(title);
+    }
+
+    @Inject(at = @At("TAIL"), method = "init()V")
+    private void injectModOptionsButton(CallbackInfo ci) {
+        this.addButton(new TexturedButtonWidget(this.width / 2 - 124, this.height / 4 + 48, 20, 20, 0, 0, 20, WIDGETS_LOCATION, 320, 320, (buttonWidget) -> {
+            assert this.client != null;
+            this.client.openScreen(new LanguageOptionsScreen(this, this.client.options, this.client.getLanguageManager()));
+        }, new TranslatableText("modoptions")));
+    }
+}
