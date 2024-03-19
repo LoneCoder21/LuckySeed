bd0ed1364694aefafd7cef09b9f2c13036967fa2
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-03-18 21:47:39 -0700

    add only landsurface option to ruined portals

--- src/main/java/name/luckyseed/luckyseed1_16_1/mixin/RuinedPortalFeatureMixin.java
+++ src/main/java/name/luckyseed/luckyseed1_16_1/mixin/RuinedPortalFeatureMixin.java
@@ -1,5 +1,6 @@
 package name.luckyseed.luckyseed1_16_1.mixin;
 
+import net.minecraft.structure.RuinedPortalStructurePiece;
 import net.minecraft.world.gen.feature.RuinedPortalFeature;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,5 +11,11 @@ public class RuinedPortalFeatureMixin {
     @ModifyVariable(method = "method_27211(Ljava/util/Random;Lnet/minecraft/world/gen/chunk/ChunkGenerator;Lnet/minecraft/structure/RuinedPortalStructurePiece$VerticalPlacement;ZIILnet/minecraft/util/math/BlockBox;)I", at = @At("HEAD"), argsOnly = true)
     private static boolean modifyAirPockets(boolean bl) {
         return true;
+    }
+
+    @ModifyVariable(method = "method_27211(Ljava/util/Random;Lnet/minecraft/world/gen/chunk/ChunkGenerator;Lnet/minecraft/structure/RuinedPortalStructurePiece$VerticalPlacement;ZIILnet/minecraft/util/math/BlockBox;)I", at = @At("HEAD"), argsOnly = true)
+    private static RuinedPortalStructurePiece.VerticalPlacement modifyPlacements(RuinedPortalStructurePiece.VerticalPlacement verticalPlacement) {
+        if (verticalPlacement == RuinedPortalStructurePiece.VerticalPlacement.IN_NETHER) return verticalPlacement;
+        return RuinedPortalStructurePiece.VerticalPlacement.ON_LAND_SURFACE;
     }
 }
--- src/main/java/name/luckyseed/luckyseed1_16_1/mixin/RuinedPortalStructurePieceMixin.java
+++ src/main/java/name/luckyseed/luckyseed1_16_1/mixin/RuinedPortalStructurePieceMixin.java
@@ -12,4 +12,16 @@ public class RuinedPortalStructurePieceMixin {
         y.airPocket = true;
         return y;
     }
+
+    @ModifyVariable(method = "<init>(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/structure/RuinedPortalStructurePiece$VerticalPlacement;Lnet/minecraft/structure/RuinedPortalStructurePiece$Properties;Lnet/minecraft/util/Identifier;Lnet/minecraft/structure/Structure;Lnet/minecraft/util/BlockRotation;Lnet/minecraft/util/BlockMirror;Lnet/minecraft/util/math/BlockPos;)V", at = @At("HEAD"), argsOnly = true)
+    private static RuinedPortalStructurePiece.VerticalPlacement modifyPlacementsCtor(RuinedPortalStructurePiece.VerticalPlacement verticalPlacement) {
+        if (verticalPlacement == RuinedPortalStructurePiece.VerticalPlacement.IN_NETHER) return verticalPlacement;
+        return RuinedPortalStructurePiece.VerticalPlacement.ON_LAND_SURFACE;
+    }
+
+    @ModifyVariable(method = "getHeightmapType(Lnet/minecraft/structure/RuinedPortalStructurePiece$VerticalPlacement;)Lnet/minecraft/world/Heightmap$Type;", at = @At("HEAD"), argsOnly = true)
+    private static RuinedPortalStructurePiece.VerticalPlacement modifyPlacements(RuinedPortalStructurePiece.VerticalPlacement verticalPlacement) {
+        if (verticalPlacement == RuinedPortalStructurePiece.VerticalPlacement.IN_NETHER) return verticalPlacement;
+        return RuinedPortalStructurePiece.VerticalPlacement.ON_LAND_SURFACE;
+    }
 }
