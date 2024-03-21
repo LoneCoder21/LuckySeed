de58ca2f9100a5b62896d50e1b29cc0df97df637
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-03-21 13:49:58 -0700

    disable random gold block replacement rule in bastions

---
+++ src/main/java/name/luckyseed/luckyseed1_16_1/mixin/RandomBlockMatchRuleTestMixin.java
@@ -0,0 +1,28 @@
+package name.luckyseed.luckyseed1_16_1.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.structure.rule.RandomBlockMatchRuleTest;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(RandomBlockMatchRuleTest.class)
+public class RandomBlockMatchRuleTestMixin {
+    @Mutable
+    @Shadow
+    @Final
+    private float probability;
+
+    @Inject(method = "<init>(Lnet/minecraft/block/Block;F)V", at = @At("TAIL"))
+    private void modifyBastionGoldBlockRule(Block block, float f, CallbackInfo ci) {
+
+        if (block == Blocks.GOLD_BLOCK) {
+            this.probability = 0.0F;
+        }
+    }
+}
