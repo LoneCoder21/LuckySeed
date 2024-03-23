c107d0a81ca0680776e25bbd19ada05b451a45e3
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-03-23 10:24:43 -0700

    fix math bugs for nether structure generation

--- src/main/java/name/luckyseed/luckyseed1_16_1/CustomStructureConfig.java
+++ src/main/java/name/luckyseed/luckyseed1_16_1/CustomStructureConfig.java
@@ -3,6 +3,6 @@ package name.luckyseed.luckyseed1_16_1;
 import net.minecraft.world.gen.chunk.StructureConfig;
 
 public class CustomStructureConfig {
-    public static StructureConfig BastionRemnant = new StructureConfig(12, 8, 30084232);
-    public static StructureConfig NetherFortress = new StructureConfig(15, 8, 30084232);
+    public static StructureConfig BastionRemnant = new StructureConfig(15, 4, 30084232);
+    public static StructureConfig NetherFortress = new StructureConfig(15, 4, 30084232);
 }
--- src/main/java/name/luckyseed/luckyseed1_16_1/mixin/BastionRemnantFeatureMixin.java
+++ src/main/java/name/luckyseed/luckyseed1_16_1/mixin/BastionRemnantFeatureMixin.java
@@ -8,6 +8,8 @@ import net.minecraft.world.gen.ChunkRandom;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.BastionRemnantFeature;
 import net.minecraft.world.gen.feature.BastionRemnantFeatureConfig;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,9 +21,12 @@ public class BastionRemnantFeatureMixin {
     @Inject(method = "shouldStartAt(Lnet/minecraft/world/gen/chunk/ChunkGenerator;Lnet/minecraft/world/biome/source/BiomeSource;JLnet/minecraft/world/gen/ChunkRandom;IILnet/minecraft/world/biome/Biome;Lnet/minecraft/util/math/ChunkPos;Lnet/minecraft/world/gen/feature/BastionRemnantFeatureConfig;)Z", at = @At("RETURN"), cancellable = true)
     private void generateDiagonalStructures(ChunkGenerator chunkGenerator, BiomeSource biomeSource, long l, ChunkRandom chunkRandom, int i, int j, Biome biome, ChunkPos chunkPos, BastionRemnantFeatureConfig bastionRemnantFeatureConfig, CallbackInfoReturnable<Boolean> cir) {
         int spacing = CustomStructureConfig.BastionRemnant.getSpacing();
-        int x = chunkPos.x / spacing;
-        int z = chunkPos.z / spacing;
-        int parity = (x + z) % 2;
+        int x = Math.floorDiv(chunkPos.x, spacing);
+        int z = Math.floorDiv(chunkPos.z, spacing);
+        int parity = (((x + z) % 2) + 2) % 2;
+        final Logger LOGGER = LogManager.getLogger("luckyseed");
+        LOGGER.info(chunkPos);
+        LOGGER.info(parity);
         cir.setReturnValue(parity == 0);
     }
 }
