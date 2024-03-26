535e39fca1d4c333eda96485cafc3ef6715f547f
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-03-25 18:38:32 -0700

    cleanup

--- src/main/java/name/luckyseed/luckyseed1_16_1/mixin/BiomeMixin.java
+++ src/main/java/name/luckyseed/luckyseed1_16_1/mixin/BiomeMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Biome.class)
 public class BiomeMixin {
     @Inject(at = @At("HEAD"), method = "addStructureFeature(Lnet/minecraft/world/gen/feature/ConfiguredStructureFeature;)V", cancellable = true)
-    private void disableMonumentFeature(ConfiguredStructureFeature<?, ?> configuredStructureFeature, CallbackInfo ci) {
+    private void modifyStructuresFeature(ConfiguredStructureFeature<?, ?> configuredStructureFeature, CallbackInfo ci) {
         if (configuredStructureFeature == DefaultBiomeFeatures.MONUMENT || configuredStructureFeature == DefaultBiomeFeatures.DESERT_RUINED_PORTAL || configuredStructureFeature == DefaultBiomeFeatures.SWAMP_RUINED_PORTAL || configuredStructureFeature == DefaultBiomeFeatures.OCEAN_RUINED_PORTAL || configuredStructureFeature == DefaultBiomeFeatures.COLD_OCEAN_RUIN || configuredStructureFeature == DefaultBiomeFeatures.WARM_OCEAN_RUIN || configuredStructureFeature == DefaultBiomeFeatures.DESERT_VILLAGE || configuredStructureFeature == DefaultBiomeFeatures.TAIGA_VILLAGE || configuredStructureFeature == DefaultBiomeFeatures.MESA_MINESHAFT || configuredStructureFeature == DefaultBiomeFeatures.NORMAL_MINESHAFT) {
             ci.cancel();
         }
