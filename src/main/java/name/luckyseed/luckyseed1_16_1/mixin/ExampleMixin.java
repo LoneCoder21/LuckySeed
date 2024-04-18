5ba9de78f5c4faff61251848f5b33b98a8b87e4f
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-04-18 14:42:59 -0700

    fix bug of sometimes spawning below ground height when chunks aren't fully generated

--- src/main/java/name/luckyseed/luckyseed1_16_1/mixin/ExampleMixin.java
+++ src/main/java/name/luckyseed/luckyseed1_16_1/mixin/ExampleMixin.java
@@ -6,28 +6,42 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.GameRules;
+import net.minecraft.world.Heightmap;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
+	@Unique
+	private static BlockPos spawnPos = null;
 	@Inject(at = @At("HEAD"), method = "prepareStartRegion(Lnet/minecraft/server/WorldGenerationProgressListener;)V")
-	private void init(CallbackInfo info) {
+	private void initPreGen(CallbackInfo info) {
 		MinecraftServer server = (MinecraftServer) (Object) this;
 		ServerWorld world = server.getOverworld();
-        BlockPos spawnPos = world.getChunkManager().getChunkGenerator().locateStructure(world, StructureOption.getRandomStructure(), new BlockPos(0, 0, 0), (int) 1e8, false);
+		spawnPos = world.getChunkManager().getChunkGenerator().locateStructure(world, StructureOption.getRandomStructure(), BlockPos.ORIGIN, (int) 1e8, false);
 
-		if (spawnPos == null) return;
-
-		world.setSpawnPos(spawnPos);
+		GameRules.BooleanRule d = world.getGameRules().get(GameRules.KEEP_INVENTORY);
+		d.set(GameRuleOptions.KEEP_INVENTORY, server);
 
 		GameRules.IntRule r = world.getGameRules().get(GameRules.SPAWN_RADIUS);
 		r.validate(("" + 40));
 		r.setValue(r, server);
 
-		GameRules.BooleanRule d = world.getGameRules().get(GameRules.KEEP_INVENTORY);
-		d.set(GameRuleOptions.KEEP_INVENTORY, server);
+		if (spawnPos == null) return;
+		world.setSpawnPos(spawnPos); // temporary spawn position near structure to generate start chunk generation there instead of origin.
+	}
+
+	@Inject(at = @At("TAIL"), method = "prepareStartRegion(Lnet/minecraft/server/WorldGenerationProgressListener;)V")
+	private void initPostGen(CallbackInfo info) {
+		MinecraftServer server = (MinecraftServer) (Object) this;
+		ServerWorld world = server.getOverworld();
+
+		if (spawnPos == null) return;
+		int ty = world.getTopY(Heightmap.Type.WORLD_SURFACE, spawnPos.getX(), spawnPos.getZ());
+		spawnPos = new BlockPos(spawnPos.getX(), ty, spawnPos.getZ());
+		world.setSpawnPos(spawnPos); // create new spawn position at ground after nearby chunks are fully loaded
 	}
 }
