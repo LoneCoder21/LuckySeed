e499daae2de98f69d4743881fc416465e33b127e
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-04-17 22:23:10 -0700

    change fabric mod json to only need resource loader and api-base. remove fabric api fully

--- src/main/java/name/luckyseed/luckyseed1_16_1/LuckySeed_1_16_1.java
+++ src/main/java/name/luckyseed/luckyseed1_16_1/LuckySeed_1_16_1.java
@@ -3,12 +3,9 @@ package name.luckyseed.luckyseed1_16_1;
 import name.luckyseed.luckyseed1_16_1.options.GameRuleOptions;
 import name.luckyseed.luckyseed1_16_1.options.GlobalOptions;
 import name.luckyseed.luckyseed1_16_1.options.ModOptions;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
 
-@Environment(EnvType.CLIENT)
-public class LuckySeed_1_16_1 implements ModInitializer  {
+public class LuckySeed_1_16_1 implements ModInitializer {
     private void setupOptions() {
         StructureOption.allowed_structures[StructureOption.Structure.BURIED_TREASURE.ordinal()].allow_structure = PersistentWriter.GLOBAL_WRITER.getBoolValue("buriedtreasure", StructureOption.allowed_structures[StructureOption.Structure.BURIED_TREASURE.ordinal()].allow_structure);
         StructureOption.allowed_structures[StructureOption.Structure.DESERT_PYRAMID.ordinal()].allow_structure = PersistentWriter.GLOBAL_WRITER.getBoolValue("desertpyramid", StructureOption.allowed_structures[StructureOption.Structure.DESERT_PYRAMID.ordinal()].allow_structure);
